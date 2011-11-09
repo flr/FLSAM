716cf8311ae30649a0b901856fd42b8b235bd3a5
Mark Payne <flr-team@flr-project.org>
2011-11-09 02:19:43 +0000

    Modified plot method to take advantage of new accessor functions

--- R/methods_plots.r
+++ R/methods_plots.r
@@ -1,23 +1,19 @@
 setMethod("plot",signature(x="FLSAM",y="missing"),
  function(x,y,...) {
     #Extract data - ssb and fbar are easy but recs harder
-    ssb.fbar.dat <- subset(x@params,name %in% c("logssb","logfbar"))
-    states <- subset(x@params,name=="U")
-    yrs    <- x@range["minyear"]:x@range["maxyear"]
-    n.states <- nrow(states)/length(yrs)
-    recs.dat    <- states[seq(start=1,by=n.states,length.out=length(yrs)),]
-    plot.dat    <- rbind(ssb.fbar.dat,recs.dat) 
-    plot.dat$year <- yrs
-
-    #Lets roll
-    plot.dat$ubnd <- exp(plot.dat$value + 1.96*plot.dat$std.dev)
-    plot.dat$lbnd <- exp(plot.dat$value - 1.96*plot.dat$std.dev)
-    plot.dat$est  <- exp(plot.dat$value)
-    plot.dat$name <- factor(plot.dat$name,levels=c("logssb","logfbar","U"))
-    levels(plot.dat$name) <- c("Spawning stock biomass","Fishing mortality","Recruitment")
+    ssb.dat <- ssb(x)
+    ssb.dat$name <- "Spawning stock biomass"
+    rec.dat <- rec(x)
+    rec.dat$name <- "Recruitment"
+    rec.dat$age <- NULL
+    fbar.dat <- fbar(x)
+    fbar.dat$name <- "Fishing mortality"
+    plot.dat    <- rbind(ssb.dat, fbar.dat,rec.dat) 
+    plot.dat$name <- factor(plot.dat$name,
+                       levels=c("Spawning stock biomass","Fishing mortality","Recruitment"))
 
     #Plot it
-    xyplot(est+ubnd+lbnd~year|name,data=plot.dat,
+    xyplot(value+ubnd+lbnd~year|name,data=plot.dat,
               prepanel=function(...) {list(ylim=range(pretty(c(0,list(...)$y))))},
               main=list(x@name,cex=0.9),
               ylab=rev(c("SSB","Fbar","Rec")),xlab="Year",
@@ -28,7 +24,7 @@ setMethod("plot",signature(x="FLSAM",y="missing"),
                    d<- split(data.frame(x=arg$x,y=arg$y),arg$groups[arg$subscripts])
                    panel.polygon(c(d$ubnd$x,rev(d$lbnd$x)),c(d$ubnd$y,rev(d$lbnd$y)),
                       col="grey",border=NA)
-                   panel.xyplot(d$est$x,d$est$y,col="black",lwd=2,type="l")
+                   panel.xyplot(d$value$x,d$value$y,col="black",lwd=2,type="l")
                      },
               scales=list(alternating=1,y=list(relation="free",rot=0)))
 })
