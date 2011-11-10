6a881a3556a39fdfd59480b46d372da9963fce41
Niels Hintzen <flr-team@flr-project.org>
2011-11-10 11:07:42 +0000

    Included health warning

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,31 @@
    cat(sprintf("FLSAM %s development version (%s)\n",
           packageDescription("FLSAM")$Version,
           packageDescription("FLSAM")$Date))
-   cat("<Insert Health Warning Here>\n")
+   cat("
+FLSAM v 0.40 (2011-11-09)
+--------------------------------------------------------------------------------
+* This package is an FLR wrapper to SAM, the State-space Assessment Model
+    developed by Anders Nielsen <an@aqua.dtu.dk>. It is intended to be used
+    only in close collaboration with experts in stock assessment modelling.
+    Correct model specification, thorough validation of model results, and model
+    diagnostics are required to obtain valid output.\n
+
+* SAM and FLSAM come with ABSOLUTELY NO WARRANTY.
+    For details regarding SAM, please consult <http://www.stockassessment.org>.
+    For details and support regarding the FLSAM package, please consult the HAWG
+    repository webpage, <http://hawg.googlecode.com>, where FLSAM is hosted, or
+    the FLSAM users mailing list <http://groups.google.com/group/FLSAM>.\n
+
+* If a 'bug' occurs, that is suspected to be caused by the assessment model
+    itself (and not the FLR-interface), then please verify that the current
+    official version of SAM available at <http://www.stockassessment.org>
+    contains the bug before contacting the author with a bug report.\n
+
+* The State-space Assessment Model (SAM) is constantly developing, so not
+    all features that are available in SAM are available through FLSAM.
+    Please consult http://www.stockassessment.org or the authors if your
+    fish stock require features not covered.
+--------------------------------------------------------------------------------")
 #  cat("---------------------------------------------------------\n")
 #  cat("* Note that FLR packages are under constant development,\n") 
 #  cat("    please report bugs to flr-team@flr-project.org.\n")
