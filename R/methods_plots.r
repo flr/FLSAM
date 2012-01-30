a4631b8a0ac46c02f150c6180f817ebc790bf438
Mark Payne <flr-team@flr-project.org>
2012-01-30 11:48:00 +0000

    Added methods for plotting FLSAMs and mixed plots combined FLSAM/FLSAMs together with FLStock

--- R/methods_plots.r
+++ R/methods_plots.r
@@ -28,3 +28,85 @@ setMethod("plot",signature(x="FLSAM",y="missing"),
                      },
               scales=list(alternating=1,y=list(relation="free",rot=0)))
 })
+
+setMethod("plot",signature(x="FLSAMs",y="missing"),
+ function(x,y,main="",...) {
+    #Extract data - ssb and fbar are easy but recs harder
+    ssb.dat <- ssb(x)
+    ssb.dat$var <- "Spawning stock biomass"
+    rec.dat <- rec(x)
+    rec.dat$var <- "Recruitment"
+    rec.dat$age <- NULL
+    fbar.dat <- fbar(x)
+    fbar.dat$var <- "Fishing mortality"
+    plot.dat    <- rbind(ssb.dat, fbar.dat,rec.dat) 
+    plot.dat$var <- factor(plot.dat$var,
+                       levels=c("Spawning stock biomass","Fishing mortality","Recruitment"))
+
+    #Plot it
+    xyplot(value~year|var,data=plot.dat,
+              prepanel=function(...) {list(ylim=range(pretty(c(0,list(...)$y))))},
+              main=main,groups=name,type="l",
+              ylab=rev(c("SSB","Fbar","Rec")),xlab="Year",
+              layout=c(1,3),as.table=TRUE,
+              auto.key=list(space="right",lines=TRUE,points=FALSE),
+              scales=list(alternating=1,y=list(relation="free",rot=0)),
+              panel=function(...) {
+                   panel.grid(h=-1,v=-1)
+                   panel.superpose.2(...)})
+})
+
+setMethod("plot",signature(x="FLSAMs",y="FLStock"),
+ function(x,y,main="",...) {
+    #extract data from FLSAMs first
+    ssb.dat <- ssb(x)
+    ssb.dat$var <- "spawning stock biomass"
+    rec.dat <- rec(x)
+    rec.dat$var <- "recruitment"
+    rec.dat$age <- NULL
+    fbar.dat <- fbar(x)
+    fbar.dat$var <- "fishing mortality"
+    sams.dat    <- rbind(ssb.dat, fbar.dat,rec.dat) 
+
+    #Then add the data from the stock
+    stck.dat <- rbind(data.frame(as.data.frame(rec(y)),var="recruitment"),
+                  data.frame(as.data.frame(ssb(y)),var="spawning stock biomass"),
+                  data.frame(as.data.frame(fbar(y)),var="fishing mortality"))
+    stck.dat$name <- y@name
+    stck.dat$value <- stck.dat$data
+    
+    #Now combine
+    common.cols <- intersect(names(stck.dat),names(sams.dat))
+    plot.dat <- rbind(stck.dat[,common.cols],sams.dat[,common.cols])
+    plot.dat$var <- factor(plot.dat$var,
+                       levels=c("spawning stock biomass","fishing mortality","recruitment"))
+
+    #plot it
+    xyplot(value~year|var,data=plot.dat,
+              prepanel=function(...) {list(ylim=range(pretty(c(0,list(...)$y))))},
+              main=main,groups=name,type="l",
+              ylab=rev(c("ssb","fbar","rec")),xlab="year",
+              layout=c(1,3),as.table=TRUE,
+              auto.key=list(space="right",lines=TRUE,points=TRUE),
+              scales=list(alternating=1,y=list(relation="free",rot=0)),
+              panel=function(...) {
+                   panel.grid(h=-1,v=-1)
+                   panel.superpose.2(...)})
+})
+
+setMethod("plot",signature(x="FLSAM",y="FLStock"),
+ function(x,y,main="",...) {
+    plot(FLSAMs(x),y,...)
+})
+
+setMethod("plot",signature(x="FLStock",y="FLSAM"),
+ function(x,y,main="",...) {
+    plot(y,x,...)
+})
+
+setMethod("plot",signature(x="FLStock",y="FLSAMs"),
+ function(x,y,main="",...) {
+    plot(y,x,...)
+})
+
+
