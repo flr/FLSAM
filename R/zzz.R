a78706c8364e6ac4538ff6d35f823b6fb85b935f
Mark Payne <flr-team@flr-project.org>
2012-08-23 09:28:46 +0000

    FLSAM 0.99-9 - the beta version! We're nearly there. Have added vignette for getting started with FLSAM, and edited that pompous piece of shit disclaimer. So close!

--- R/zzz.R
+++ R/zzz.R
@@ -3,30 +3,18 @@ packageStartupMessage(sprintf("\nFLSAM %s (%s)",packageDescription("FLSAM")$Vers
            packageDescription("FLSAM")$Date))
 dis.msg <- '--------------------------------------------------------------------------------
 * This package is an FLR wrapper to SAM, the State-space Assessment Model
-    developed by Anders Nielsen <an@aqua.dtu.dk>. It is intended to be used
-    only in close collaboration with experts in stock assessment modelling.
-    Correct model specification, thorough validation of model results, and model
-    diagnostics are required to obtain valid output.
+    developed by Anders Nielsen, DTU-Aqua. For details regarding SAM, 
+    please consult <http://www.stockassessment.org>.
 
 * SAM and FLSAM come with ABSOLUTELY NO WARRANTY.
 
-* For details regarding SAM, please consult <http://www.stockassessment.org>.
-    For details and support regarding the FLSAM package, please consult the FLSAM
-    wiki, <http://code.google.com/p/hawg/wiki/FLSAM>, where FLSAM is hosted, or
-    the associated FLSAM users mailing list <http://groups.google.com/group/FLSAM>.
+* Please report all bugs, questions and feature requestions to the FLR
+    mailing list, flr-list@flr-project.org. We´re here to help! 
 
-* If a "bug" occurs, that is suspected to be caused by the assessment model
-    itself (and not the FLR-interface), then please verify that the current
-    official version of SAM available at <http://www.stockassessment.org>
-    contains the bug before contacting the author with a bug report.
-
-* The State-space Assessment Model (SAM) is constantly developing, so not
-    all features that are available in SAM are available through FLSAM.
-    Please consult <http://www.stockassessment.org> or the authors if your
-    fish stock requires features not covered.
-
-* For help on FLSAM, try help(package="FLSAM"). For the full manual as a PDF file, 
-  use vignette("FLSAM")
+* For more information on learning to use FLSAM, run vignette("FLSAM"). For 
+    the full manual as a PDF file, run vignette("FLSAM-manual"). Full
+    documentation is also availabe through the standard R help system - use
+    help(package="FLSAM") to get started there.
 --------------------------------------------------------------------------------
 '
 packageStartupMessage(dis.msg)
