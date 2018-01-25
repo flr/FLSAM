847ff870d44fb83b417fb15c93fefde9ec5f2320
nielshintzen <niels.hintzen@gmail.com>
2018-01-25 22:35:00 +0100

    updating retro function to run for multifleet

--- R/looi_retro.r
+++ R/looi_retro.r
@@ -162,3 +162,70 @@ function(stock, indices, control, retro, year.range="missing"){
     return(res) 
   })  #End setMethod
 
+setMethod('retro', signature(stock='FLStocks', indices='FLIndices', control='FLSAM.control',retro='numeric'),
+function(stock, indices, control, retro, year.range="missing"){
+
+  # ---------- Checks ----------------------
+    if (!inherits(stock, "FLStocks"))
+      stop("stock must be an 'FLStocks' object!")
+    if (!validObject(stock))
+      stop("stocks is not a valid object!")
+    if (!inherits(indices, "FLIndices"))
+      stop("indices must be an 'FLIndices' object!")
+    if (!validObject(indices))
+      stop("indices is not a valid object!")
+    # Check we have at least a usable retro or years.range.
+    if ((missing(year.range) || !is.numeric(year.range)) && (!is.numeric(retro) || retro < 0 || is.na(retro) || length(retro) > 1))
+      stop("Either 'retro' argument must be a non-negative integer or 'year.range' must be a numeric vector.")
+    # ------------ Sort out retrospective years over which to perform assessment ------------
+    
+    stck.min.yr <- min(unlist(lapply(stock,function(x){return(x@range["minyear"])})))
+    stck.max.yr <- max(unlist(lapply(stock,function(x){return(x@range["maxyear"])})))
+    stcks.range <- lapply(stock,function(x){return(x@range[c("minyear","maxyear")])})
+    trim.stock  <- which.max(do.call(rbind,stcks.range)[,"maxyear"])
+    
+    if(missing(year.range))
+      year.range <- (stck.max.yr-retro):stck.max.yr
+    if(min(year.range)<= stcks.range[[trim.stock]]["minyear"])
+      stop("Running retrospective beyond combination of sum and residual fleets")
+    # Calculate hangover years - but only consider positive hangovers
+    hangover.yrs <- sapply(indices,function(x) dims(x)$maxyear) - stck.max.yr
+    hangover.yrs <- ifelse(hangover.yrs < 0,0,hangover.yrs)
+    # Check year.range is sensible
+    if(min(year.range) < stck.min.yr || max(year.range) > stck.max.yr)
+      stop("Year range outside stock object range")
+    # ---------- Run that retrospective -------------
+    cat("Running retrospective...\n")
+    res <- new("FLSAMs")
+    for (yr in rev(year.range)){  #yr is the year in which the assessment is being simulated
+      Stock <- new("FLStocks")
+      for(iStk in 1:length(stock)){
+        if(iStk == trim.stock)
+          Stock[[iStk]] <- trim(stock[[trim.stock]], year=stcks.range[[trim.stock]]["minyear"]:yr)
+        if(iStk != trim.stock)
+          Stock[[iStk]] <- stock[[iStk]]
+      }
+      names(Stock) <- names(stock)
+      Indices.temp<-indices
+      for (j in 1:length(indices)) {
+        idx.min.yr <- dims(indices[[j]])$minyear
+        if (yr < idx.min.yr) {stop(paste("Year requested (",yr,
+              ") is earlier than the first year (",
+              idx.min.yr,") of the ",indices[[j]]@name," index.",sep="")) }
+        idx.max.yr <- min(dims(indices[[j]])$maxyear, yr+hangover.yrs[j])
+        Indices.temp[[j]] <- trim(indices[[j]],year=idx.min.yr:idx.max.yr)
+      }
+      control@name <- as.character(yr)
+      control@range["maxyear"] <- max(max(unlist(lapply(Stock,function(x){return(x@range["maxyear"])}))),
+                max(sapply(Indices.temp,function(x) max(x@range[c("maxyear")]))))
+      assess  <- FLSAM(Stock, Indices.temp,control)
+      if(is.null(assess)) {
+        warning(sprintf("Retrospective for year %i failed\n",yr))
+      } else {
+        assess@desc    <-  paste(as.character(yr), "Retrospective")
+        res[[as.character(yr)]] <- assess
+      }
+    }
+    res@desc   <- paste("Retrospective analysis from object", stock@desc)
+    return(res)
+  })  #End setMethod
