0427ecd767a066da303a51913bf3ca3ed1a3d492
Iago Mosqueira <iago@mosqueira.eu>
2022-09-28 10:39:45 +0200

    Added warning if stockassessment already loaded

--- R/zzz.R
+++ R/zzz.R
@@ -5,8 +5,9 @@ packageStartupMessage(sprintf("\nFLSAM %s (%s)",packageDescription("FLSAM")$Vers
 "------------------------------------------------------------------------------
 
 * NOTE: This version of FLSAM requires a particular version of the
-    'stockassessment' package. Please install using
-	  devtools::install_github('fishfollower/SAM/stockassessment', ref='components')
+    'stockassessment' package available as package 'stockassessmentComp'.
+    Both 'stockassessment' and 'stockassessmentComp' can be installed in
+    the same machine, but should not be loaded in the same R session.
 
 * This package is an FLR wrapper to SAM, the State-space Assessment Model
     developed by Anders Nielsen, DTU-Aqua. For details regarding SAM, 
@@ -22,6 +23,11 @@ packageStartupMessage(sprintf("\nFLSAM %s (%s)",packageDescription("FLSAM")$Vers
     documentation is also availabe through the standard R help system - use
     help(package='FLSAM') to get started there.
 ------------------------------------------------------------------------------")
+
+
+if("stockassessment" %in% (.packages())){
+  warning("'stockassessment' already attached, functions will be masked.")
+}
 
 if(packageVersion("stockassessmentComp") != "0.5.4")
   stop("Version of stockassessment is not compatible with FLSAM, see above.")
